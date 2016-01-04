83a6f9115c778a44c4a401cc10e1849715fd00d1
Illia Kuchko and Nathan Romike <illia+nathan@>
2016-01-04 10:26:53 -0800

    Red Green on first two tests, red stage on 3rd test.

---
+++ src/main/java/LeapYear.java
@@ -0,0 +1,13 @@
+public class LeapYear {
+  public static void main (String[] args) {}
+
+  public Boolean isLeapYear(Integer year){
+    if (year % 100 == 0) {
+      return false;
+    } else if (year % 4 == 0) {
+      return true;      
+    } else {
+      return false;
+    }
+  }
+}
